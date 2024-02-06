cb5222e96aa0b153e3e92a8d21a86ccce7e5d01a
yucheng <13258137734@163.com>
2024-02-06 15:44:42 +0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # yc-cli
+
+autocannon -c 100 -d 5 https://app.konnect.chat/
--- bin/yc.js
+++ bin/yc.js
@@ -2,13 +2,39 @@
 
 const { Command } = require('commander');
 const program = new Command();
+const inquirer = require('inquirer')
 
 program
     .name('yc')
     .description('余成的个人cli 啦啦啦啦')
     .version('1.0.0')
-    .action(()=>{
+    .action(() => {
         console.log('欢迎使用余成的个人cli');
+        inquirer.prompt([
+            {
+                type: 'list', //type： input, number, confirm, list, checkbox ... 
+                name: 'name', // key 名
+                message: '第一步', // 提示信息
+                default: 'yc-cli', // 默认值
+                choices: ["a", "b"],
+            },
+        ]).then(answers => {
+            // 打印互用输入结果
+            console.log(answers)
+
+            inquirer.prompt([
+                {
+                    type: 'list', //type： input, number, confirm, list, checkbox ... 
+                    name: 'name', // key 名
+                    message: '第二步', // 提示信息
+                    default: 'yc-cli', // 默认值
+                    choices: ["a", "b"],
+                },
+            ]).then(answers => {
+                // 打印互用输入结果
+                console.log(answers)
+            })
+        })
     });
 
 program.command('split')
