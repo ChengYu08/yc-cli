7206e258c0ce9e8265b68a1bb0d9217b74d8e6ba
yucheng <13258137734@163.com>
2024-02-23 12:25:34 +0800

    1

--- bin/yc.js
+++ bin/yc.js
@@ -1,16 +1,34 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
-const program = new Command();
 import inquirer from "inquirer";
-
 import transpile from "../lib/png_to_webp.js";
 import lang from "../lib/lang.js";
 import csdn from "../lib/csdn.js"
 
+
+// const gradient = require('gradient-string');
+
+import gradient from "gradient-string";
+
+console.log(gradient([ 
+    {color: '#42d392', pos: 0},
+    {color: '#42d392', pos: 0.1},
+    {color: '#647eff', pos: 1 }
+])('yc cli - Individual Efficiency Scaffolding'));
+
+const program = new Command();
+
+
+// 版本
+program.version("0.0.2", '-v, --version', '@calmer/terminal-cli 当前版本')
+
+
+
+// import cliSpinners from "cli-spinners";
+
 // 获取当前工作目录
-const currentDirectory = process.cwd();
-console.log('当前工作目录:', currentDirectory);
+// const currentDirectory = process.cwd();
 
 // // 获取执行脚手架命令的目录
 // const commandDirectory = path.dirname(process.argv[1]);
@@ -19,7 +37,6 @@ console.log('当前工作目录:', currentDirectory);
 program
     .name('yucheng')
     .description('余成的个人cli 啦啦啦啦')
-    .version('0.0.1')
     .action(() => {
         console.log('欢迎使用余成的个人cli');
         inquirer.prompt([
@@ -93,5 +110,8 @@ program.command('split')
         const limit = options.first ? 1 : undefined;
         console.log(str.split(options.separator, limit));
     });
+
+
+
 
 program.parse();
